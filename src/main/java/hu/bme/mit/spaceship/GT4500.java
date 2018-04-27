054f4eae40cde168f07c60b57702c9de8e596672
Dávid Csendes <sylent@hotmail.hu>
2018-04-26 23:49:26 -0700

    Replacing switch with if statement in GT4500

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -38,8 +38,7 @@ public class GT4500 implements SpaceShip {
 
     boolean firingSuccess = false;
 
-    switch (firingMode) {
-      case SINGLE:
+   if(firingMode == FiringMode.SINGLE){
         if (wasPrimaryFiredLast) {
           // try to fire the secondary first
           if (! secondaryTorpedoStore.isEmpty()) {
@@ -74,9 +73,8 @@ public class GT4500 implements SpaceShip {
             // if both of the stores are empty, nothing can be done, return failure
           }
         }
-        break;
-
-      case ALL:
+   }
+   if(firingMode == FiringMode.ALL){
         // try to fire both of the torpedo stores
 	if(!primaryTorpedoStore.isEmpty()){
 		firingSuccess = primaryTorpedoStore.fire(1);
@@ -84,7 +82,6 @@ public class GT4500 implements SpaceShip {
 	if(!secondaryTorpedoStore.isEmpty()){
 		firingSuccess = secondaryTorpedoStore.fire(1) || firingSuccess;
 	}
-        break;
     }
 
     return firingSuccess;
