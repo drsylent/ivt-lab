07b19b42b4f403240f9825de47c0e683fc89cad8
Csendes Dávid <sylent@hotmail.hu>
2018-05-10 23:36:50 -0700

    Using mockito in the tests

--- src/test/java/hu/bme/mit/spaceship/GT4500Test.java
+++ src/test/java/hu/bme/mit/spaceship/GT4500Test.java
@@ -8,16 +8,21 @@ import org.junit.Test;
 
 public class GT4500Test {
 
+  private TorpedoStore mockTSPrimary;
+  private TorpedoStore mockTSSecondary;
   private GT4500 ship;
 
   @Before
   public void init(){
-    this.ship = new GT4500();
+	mockTSPrimary = mock(TorpedoStore.class);
+	mockTSSecondary = mock(TorpedoStore.class);
+    this.ship = new GT4500(mockTSPrimary, mockTSSecondary);
   }
 
   @Test
   public void fireTorpedo_Single_Success(){
     // Arrange
+	when(mockTSPrimary.fire(1)).thenReturn(true);
 
     // Act
     boolean result = ship.fireTorpedo(FiringMode.SINGLE);
@@ -29,6 +34,8 @@ public class GT4500Test {
   @Test
   public void fireTorpedo_All_Success(){
     // Arrange
+	when(mockTSPrimary.fire(1)).thenReturn(true);
+	when(mockTSSecondary.fire(1)).thenReturn(true);
 
     // Act
     boolean result = ship.fireTorpedo(FiringMode.ALL);
